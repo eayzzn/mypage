247cb0d40a9d721d3f50ab0afd6043efbbf4b1eb
eayzzn <evremyaici@outlook.de>
2023-10-05 23:57:36 +0200

    more text, changed title, another div added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  
-  return (
 
-    <div className="App">
-     <h1>hallo test 11111111111</h1>
-     <h2>Hier könnte Werbung stehen</h2>
+
+function App() {
+  useEffect(() => {
+    document.title = "eayzzn";
+  }, []);
+
+  return (
+    <div>
+      <div className="Head">
+        <h1>eayzzn - Homepage</h1>
+      </div>
+      <div className="App">
+        <h1>hallo test 11111111111</h1>
+        <h2>Hier könnte Werbung stehen</h2>
+        <h3>4</h3>
+      </div>
     </div>
+
+
   );
-  }
+
+  
+}
+
 export default App;

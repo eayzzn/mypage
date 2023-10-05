d98a5c37ff996ce0343851d867ae6f5b434daea3
eayzzn <evremyaici@outlook.de>
2023-10-05 23:03:43 +0200

    New Colomn

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  }
+  
   return (
 
     <div className="App">
-     <h1>hallo test 11111</h1>
+     <h1>hallo test 11111111111</h1>
+     <h2>Hier könnte Werbung stehen</h2>
     </div>
   );
-
+  }
 export default App;
